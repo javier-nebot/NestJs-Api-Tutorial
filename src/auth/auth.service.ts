ce95a6047930e84d6755cfcf5d450551efa24c6f
Javier Nebot <javier.nebot@gmail.com>
2023-06-21 10:58:00 +0200

    SignIn logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -34,7 +34,27 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return { msg: 'I have signed in'}
+  async signin(dto: AuthDto) {
+
+    // find user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      }
+    })
+    // if user does not exist throw exception
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+
+    // compare password
+    const pwMatches = await argon.verify(
+      user.hash,
+      dto.password,
+      );
+    // if password incorrect thrown exception
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
+
+    // if all ok send back user
+    delete user.hash;
+    return user;
   }
 }
