551aed6847a2804ec2514b0f087034aeba910ce8
Javier Nebot <javier.nebot@gmail.com>
2023-06-23 16:49:47 +0200

    User e2e test

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
   
   @Get('me')
   getMe(@GetUser() user: User) {
@@ -13,7 +16,10 @@ export class UserController {
   }
 
   @Patch()
-  editUser() {
-    
+  editUser(
+    @GetUser('id') userId: number,
+    @Body() dto: EditUserDto
+  ) {
+    return this.userService.editUser(userId, dto);
   }
 }
