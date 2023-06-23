8754e1cef3ce47e3e5b3bee70dea145bb858716c
Javier Nebot <javier.nebot@gmail.com>
2023-06-23 11:15:03 +0200

    e2e test wt pactum,db-test,aut db-test, dotenv-cli

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,10 @@
-import { ForbiddenException, Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from "./dto";
 import * as argon from 'argon2'
-import { Prisma } from "@prisma/client";
-import { JwtService } from "@nestjs/jwt";
-import { ConfigService } from "@nestjs/config";
+import { Prisma } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
